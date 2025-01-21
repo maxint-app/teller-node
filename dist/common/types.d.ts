bcdff02ad2b91ecafeb3ea2fb901d688296a2dfd
MatthewGould123 <matt@saowapan.com>
2025-01-21 12:40:55 +0700

    build and publish

--- dist/common/types.d.ts
+++ dist/common/types.d.ts
@@ -1,8 +1,20 @@
 export type TellerOptionsBase = {
     accessToken: string;
 };
+/**
+ * Using the latest Teller Version: 2020-10-12
+ */
 export type TellerOptionsPagination = TellerOptionsBase & {
-    cursor: string;
-    limit: number;
+    /**
+     * The maximum number of transactions to return in the API response.
+     */
+    count: number;
+    /**
+     * The transaction from where to start the page.
+     * The first transaction in the API response will be the one immediately before the transaction in the ledger with this id.
+     *
+     * example: txn_oiluj93igokseo0i3a005
+     */
+    from_id: string;
 };
 //# sourceMappingURL=types.d.ts.map
